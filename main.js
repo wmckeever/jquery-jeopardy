17c687ddbb91afcf01f07e408b5b7b90be447245
wmckeever <wil.mckeever@gmail.com>
2022-11-28 12:30:25 -0500

    initial commit with dependencies added

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// read the local jeopardy json file with async await
+// 1. Pull the promise then 2. parse the data 
+let readJeopardyData = async () => {
+    let rawJeopardyData = await fetch('jeopardy.json');
+
+    let data = await rawJeopardyData.json();
+    console.log(data);
+
+    let groupedData = _.groupBy(data, 'value');
+    console.log(groupedData);
+    console.log(groupedData.$200);
+}
+readJeopardyData();
