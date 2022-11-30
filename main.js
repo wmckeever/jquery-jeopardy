92aae038891023a33091b6abfb2d23d60654ff43
wmckeever <wil.mckeever@gmail.com>
2022-11-30 11:32:33 -0500

    fixed score not reading as number issue and added tweaks to interface design - fully functional at this poinst

--- main.js
+++ main.js
@@ -11,13 +11,6 @@ let thirdRowQuestions = document.querySelectorAll('#row3 .col');
 let fourthRowQuestions = document.querySelectorAll('#row4 .col');
 let fifthRowQuestions = document.querySelectorAll('#row5 .col');
 
-// let question101 = document.querySelector('.q-101');
-// let question102 = document.querySelector('.q-102');
-// let question103 = document.querySelector('.q-103');
-// let question104 = document.querySelector('.q-104');
-// let question105 = document.querySelector('.q-105');
-
-
 let question = document.querySelector('#question');
 let userInput = document.querySelector('input');
 let submit = document.querySelector('button')
@@ -25,6 +18,7 @@ let submit = document.querySelector('button')
 let scoreSum = 0;
 let questionColor = false;
 
+userInput.value = "";
 
 // read the local jeopardy json file with async await
 // 1. Pull the promise then 2. parse the data 
@@ -42,82 +36,132 @@ let readJeopardyData = async () => {
 //*<------------ eventListeners for the questions ------------>*//
 //*<--------------------------------------------------------->*//
   
-
-
-// // question highlight
-// for (let i = 0; i < allQuestions.length; i++) {
-//   allQuestions[i].addEventListener('mouseenter', function(){
-//     if(questionColor = false) {
-        
-//     }
-    
-//   }) 
-// };
-
-
-
-
-// First row
+//* First row
     for (let i = 0; i < firstRowQuestions.length; i++) {
       firstRowQuestions[i].addEventListener('click', function(){
       
         let random100 =  groupedData.$100[Math.round(Math.random() * groupedData.$100.length)];
         question.innerHTML = `${random100.question}`
+        
         console.log(random100.question);
         console.log(random100.answer);
-        // console.log(typeof(random100.value));
-
+       
+        
+        //*<----- submit button code ----->*//
         submit.addEventListener('click', function(event){
           event.preventDefault();
-          console.log("The submit button is working")
-          console.log("The user's answer is:" +  userInput.value)
-        
-                if(userInput.value.toLowerCase() === random100.answer.toLowerCase()){
-                  scoreSum += scoreSum + Number(random100.value);
+            console.log(userInput.value);
+            console.log(random100.answer.toLowerCase())
+
+            if(userInput.value.toLowerCase() === random100.answer.toLowerCase()){
+                  scoreSum += parseInt(random100.value.substr(1));
                   question.innerHTML = "Correct!"
-                  scoreAmount.innerHTML = `${scoreSum}`;
-                  console.log("random test");
+                  scoreAmount.innerHTML = `$${scoreSum}`;
+                  firstRowQuestions[i].style.backgroundColor = "gold"; 
+                  firstRowQuestions[i].style.color = "blue"; 
+                  firstRowQuestions[i].style.textDecoration = "line-through"; 
+                  firstRowQuestions[i].style.textDecorationColor = "red"; 
+                  userInput.value = "";
                 }else {
                   question.innerHTML = "Incorrect. Try again."
-                  console.log("random test 2");
-                }
-                
-        
-          userInput.value = "";
-          // question.innerHTML = "The question will appear here:";
+                  }
+                                  
         });  
-         
-                  
-
+             
       }) 
   };
-      // Second row 
+      //* Second row 
     for (let i = 0; i < secondRowQuestions.length; i++) {
       secondRowQuestions[i].addEventListener('click', function(){
       
         let random200 = groupedData.$200[Math.round(Math.random() * groupedData.$200.length)];
         question.innerHTML = `${random200.question}`
         console.log(random200.question);
+        console.log(random200.answer);
+
+            //*<----- submit button code ----->*//
+            submit.addEventListener('click', function(event){
+              event.preventDefault();
+                console.log(userInput.value);
+                console.log(random200.answer.toLowerCase())
+
+                if(userInput.value.toLowerCase() === random200.answer.toLowerCase()){
+                      scoreSum += parseInt(random200.value.substr(1));
+                      question.innerHTML = "Correct!"
+                      scoreAmount.innerHTML = `$${scoreSum}`;
+                      secondRowQuestions[i].style.backgroundColor = "gold"; 
+                      secondRowQuestions[i].style.color = "blue"; 
+                      secondRowQuestions[i].style.textDecoration = "line-through"; 
+                      secondRowQuestions[i].style.textDecorationColor = "red"; 
+                      userInput.value = "";
+                    }else {
+                      question.innerHTML = "Incorrect. Try again."
+                    }     
+            });  
       }) 
   };
-      // Third row 
+
+
+      //* Third row 
     for (let i = 0; i < thirdRowQuestions.length; i++) {
       thirdRowQuestions[i].addEventListener('click', function(){
       
         let random400 = groupedData.$400[Math.round(Math.random() * groupedData.$400.length)];
         question.innerHTML = `${random400.question}`
         console.log(random400.question);
+        console.log(random400.answer);
+
+        //*<----- submit button code ----->*//
+        submit.addEventListener('click', function(event){
+          event.preventDefault();
+            console.log(userInput.value);
+            console.log(random400.answer.toLowerCase())
+
+            if(userInput.value.toLowerCase() === random400.answer.toLowerCase()){
+                  scoreSum += parseInt(random400.value.substr(1));
+                  question.innerHTML = "Correct!"
+                  scoreAmount.innerHTML = `$${scoreSum}`;
+                  secondRowQuestions[i].style.backgroundColor = "gold"; 
+                  secondRowQuestions[i].style.color = "blue"; 
+                  secondRowQuestions[i].style.textDecoration = "line-through"; 
+                  secondRowQuestions[i].style.textDecorationColor = "red"; 
+                  userInput.value = "";
+                }else {
+                  question.innerHTML = "Incorrect. Try again."
+                }     
+        });  
       }) 
   };
-      // Fourth row 
+      //* Fourth row 
     for (let i = 0; i < fourthRowQuestions.length; i++) {
       fourthRowQuestions[i].addEventListener('click', function(){
       
         let random600 = groupedData.$600[Math.round(Math.random() * groupedData.$600.length)];
         question.innerHTML = `${random600.question}`
         console.log(random600.question);
+        console.log(random600.answer);
+
+              //*<----- submit button code ----->*//
+            submit.addEventListener('click', function(event){
+              event.preventDefault();
+                console.log(userInput.value);
+                console.log(random600.answer.toLowerCase())
+
+                if(userInput.value.toLowerCase() === random600.answer.toLowerCase()){
+                      scoreSum += parseInt(random600.value.substr(1));
+                      question.innerHTML = "Correct!"
+                      scoreAmount.innerHTML = `$${scoreSum}`;
+                      secondRowQuestions[i].style.backgroundColor = "gold"; 
+                      secondRowQuestions[i].style.color = "blue"; 
+                      secondRowQuestions[i].style.textDecoration = "line-through"; 
+                      secondRowQuestions[i].style.textDecorationColor = "red"; 
+                      userInput.value = "";
+                    }else {
+                      question.innerHTML = "Incorrect. Try again."
+                    }     
+            });  
       }) 
-  };
+  }; 
       // Fourth row 
     for (let i = 0; i < fifthRowQuestions.length; i++) {
       fifthRowQuestions[i].addEventListener('click', function(){
@@ -125,6 +169,27 @@ let readJeopardyData = async () => {
         let random800 = groupedData.$800[Math.round(Math.random() * groupedData.$800.length)];
         question.innerHTML = `${random800.question}`
         console.log(random800.question);
+        console.log(random800.answer);
+
+        //*<----- submit button code ----->*//
+        submit.addEventListener('click', function(event){
+          event.preventDefault();
+            console.log(userInput.value);
+            console.log(random800.answer.toLowerCase())
+
+            if(userInput.value.toLowerCase() === random800.answer.toLowerCase()){
+                  scoreSum += parseInt(random800.value.substr(1));
+                  question.innerHTML = "Correct!"
+                  scoreAmount.innerHTML = `$${scoreSum}`;
+                  secondRowQuestions[i].style.backgroundColor = "gold"; 
+                  secondRowQuestions[i].style.color = "blue"; 
+                  secondRowQuestions[i].style.textDecoration = "line-through"; 
+                  secondRowQuestions[i].style.textDecorationColor = "red"; 
+                  userInput.value = "";
+                }else {
+                  question.innerHTML = "Incorrect. Try again."
+                }     
+        });  
       }) 
   };
 
